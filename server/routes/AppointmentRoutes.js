e3ff362524af2f7d5187ba5fda360a6549a7c986
BhanuPrasad-Boda <sivabhanuboda@gmail.com>
2026-01-04 18:43:14 +0530

    ..

--- server/routes/AppointmentRoutes.js
+++ server/routes/AppointmentRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Appointment = require("../models/Appointment");
- const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 
 // 🔹 GET single appointment (protected)
