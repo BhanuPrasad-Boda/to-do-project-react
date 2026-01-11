35a1035413c592b8f76af705a237660019713359
BhanuPrasad-Boda <sivabhanuboda@gmail.com>
2026-01-11 19:45:05 +0530

    ../

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,71 @@
 const Loader = ({ message }) => {
   return (
-      <div className="d-flex justify-content-center align-items-center">
-        <span class="loader"></span>
-      </div>
+      <div class="preloader">
+  <div class="preloader__ring">
+    <div class="preloader__sector">L</div>
+    <div class="preloader__sector">o</div>
+    <div class="preloader__sector">a</div>
+    <div class="preloader__sector">d</div>
+    <div class="preloader__sector">i</div>
+    <div class="preloader__sector">n</div>
+    <div class="preloader__sector">g</div>
+    <div class="preloader__sector">.</div>
+    <div class="preloader__sector">.</div>
+    <div class="preloader__sector">.</div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+  </div>
+  <div class="preloader__ring">
+    <div class="preloader__sector">L</div>
+    <div class="preloader__sector">o</div>
+    <div class="preloader__sector">a</div>
+    <div class="preloader__sector">d</div>
+    <div class="preloader__sector">i</div>
+    <div class="preloader__sector">n</div>
+    <div class="preloader__sector">g</div>
+    <div class="preloader__sector">.</div>
+    <div class="preloader__sector">.</div>
+    <div class="preloader__sector">.</div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+    <div class="preloader__sector"></div>
+  </div>
+</div>
   );
 };
 
